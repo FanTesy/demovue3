b741c484598de06080d6f5867cf357c14a64a5fc
fantesy <huawuse73@163.com>
2021-01-19 18:18:24 +0800

    提交

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,29 @@ module.exports = {
     index: 'src/main.js'
   },
   // 保存时eslint-loader检查
-  lintOnSave: true,
+  lintOnSave: false,
+  devServer: {
+    open: false, //浏览器自动打开页面
+    host: "0.0.0.0", //如果是真机测试，就使用这个IP
+    port: 8881,
+    https: false,
+    hotOnly: false, //热更新（webpack已实现了，这里false即可）
+    proxy: {
+      //配置跨域
+      '/api': {
+        target: "http://192.168.1.1:8086",
+        ws: true,
+        changOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    },
+    overlay: {
+      warnings: false,
+      errors: false
+    }
+  },
   // 是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。
   runtimeCompiler: false,
   // 默认情况下 babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来。
